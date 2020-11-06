4fd660d86121879723edcbc41c0b32b792a4a92e
Os24 <os.deleyja@gmail.com>
2020-11-05 22:09:37 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts  from './components/Posts'
+
+function App() {
+  return (
+  <>
+  <div className={"card-container"}>
+  <Posts></Posts>
+  </div>
+  </>
+  );
+}
+
+export default App;
